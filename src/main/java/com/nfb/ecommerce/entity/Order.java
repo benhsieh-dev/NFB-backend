4d57c42627ff15fd89cabe65ba72e9e65995123e
benhsieh-dev <bqh5026@gmail.com>
2021-02-22 12:51:16 -0500

    Add order and order item entities

---
+++ src/main/java/com/nfb/ecommerce/entity/Order.java
@@ -0,0 +1,54 @@
+package com.nfb.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "order_tracking_number")
+    private String orderTrackingNumber;
+
+    @Column(name = "total_quantity")
+    private int totalQuantity;
+
+    @Column(name = "total_price")
+    private BigDecimal total_price;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+
+//    private Customer customer;
+
+//    private Long billingAddress;
+//
+//    private Long shippingAddressId;
+
+
+
+
+
+
+}
