3feef52f88e5138a916d726550ead00c91ca8d53
benhsieh-dev <bqh5026@gmail.com>
2021-02-22 12:22:22 -0500

    Add customer and address entities to match MySQL database

---
+++ src/main/java/com/nfb/ecommerce/entity/Address.java
@@ -0,0 +1,35 @@
+package com.nfb.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "street")
+    private String Street;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+//    private Order order;
+}
