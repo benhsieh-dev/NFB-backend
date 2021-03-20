e7d5a4dd2f3c0091b79ddc6d43fcf940c9e2a8ec
benhsieh-dev <bqh5026@gmail.com>
2021-03-19 22:43:16 -0400

    Add order tracking story

--- src/main/java/com/nfb/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/nfb/ecommerce/config/MyDataRestConfig.java
@@ -1,9 +1,6 @@
 package com.nfb.ecommerce.config;
 
-import com.nfb.ecommerce.entity.Country;
-import com.nfb.ecommerce.entity.Product;
-import com.nfb.ecommerce.entity.ProductCategory;
-import com.nfb.ecommerce.entity.State;
+import com.nfb.ecommerce.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -39,6 +36,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
         disableHttpMethods(State.class, config, theUnsupportedActions);
         disableHttpMethods(Country.class, config, theUnsupportedActions);
+        disableHttpMethods(Order.class, config, theUnsupportedActions);
 
         exposeIds(config);
 
